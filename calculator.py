1ec6891d04d3adcae7ff71e09e1a29ecb1f0757c
M-Ahmad-69 <m.ahmad.imran69@gmail.com>
2026-07-29 15:06:29 +0500

    Completed Lecture 0 paractice

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Welcome to my calculator! ")
+
+a = float(input("Enter a number: "))
+b = float(input("Enter second number: "))
+
+print(f"Sum: {a + b}")
+print(f"Difference = {(a - b) :.2f}")
+print(f"Product = {(a * b) :.2f}")
+print(f"Division = {(a / b ):.2f}")
---
+++ mini_profile.py
@@ -0,0 +1,6 @@
+name = input("Name: ").strip().title()
+age = int(input("Age: "))
+uni = input("University: ").strip().capitalize()
+language = input("Favorite Language: ").strip()
+
+print("Your details are stored successfully! ")
